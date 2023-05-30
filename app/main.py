7e43c41a143b6d4ce51e35cb03b2e0bb8562576c
expo180 <badersaadousalissou@gmail.com>
2023-05-30 12:43:53 +0200

    bank setup

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 from . import db
 from .forms import LoginForm
 from flask_login import login_required, current_user
-from models import BankAccount
+from .models import BankAccount
 
 main = Blueprint('main', __name__)
 
